4082c70d56d826d21c9dda45910b83755a1ce74c
Jarosław Wygoda <jaroslaw@wygoda.me>
2021-02-13 18:57:00 +0000

    neovim: add rnvimr

--- users/profiles/development/neovim/default.nix
+++ users/profiles/development/neovim/default.nix
@@ -1,6 +1,16 @@
 { pkgs, ... }:
 let
   alacritty = "${pkgs.alacritty}/bin/alacritty";
+  rnvimr = pkgs.vimUtils.buildVimPlugin {
+    name = "rnvimr";
+    passthru.python3Dependencies = ps: with ps; [ pynvim ];
+    src = pkgs.fetchFromGitHub {
+      owner = "kevinhwang91";
+      repo = "rnvimr";
+      rev = "0414b376d93ab81c6ae4e32d113e458d2b2ca46d";
+      sha256 = "sha256-LK4NIFKFhc7PzD+MYNNR2YY46hJrD7uF2CZPbl5l4Z0=";
+    };
+  };
 in
 {
   home.sessionVariables = {
@@ -38,35 +48,43 @@ in
       nnoremap <F11> :TagbarToggle<CR>
       " Use deoplete.
       let g:deoplete#enable_at_startup = 1
-      " https://github.com/rafaqz/ranger.vim/
-      let g:ranger_terminal = '${alacritty} -e'
-      nnoremap <Leader>rr :RangerEdit<cr>
-      nnoremap <Leader>rv :RangerVSplit<cr>
-      nnoremap <Leader>rs :RangerSplit<cr>
-      nnoremap <Leader>rt :RangerTab<cr>
-      nnoremap <Leader>ri :RangerInsert<cr>
-      nnoremap <Leader>ra :RangerAppend<cr>
-      nnoremap <Leader>rc :set operatorfunc=RangerChangeOperator<cr>g@
-      nnoremap <Leader>rd :RangerCD<cr>
-      nnoremap <Leader>rld :RangerLCD<cr>
+      " https://github.com/kevinhwang91/rnvimr
+      tnoremap <silent> <M-i> <C-\><C-n>:RnvimrResize<CR>
+      nnoremap <silent> <M-o> :RnvimrToggle<CR>
+      tnoremap <silent> <M-o> <C-\><C-n>:RnvimrToggle<CR>
+      " Make Ranger replace Netrw and be the file explorer
+      let g:rnvimr_enable_ex = 1
+      " Make Ranger to be hidden after picking a file
+      let g:rnvimr_enable_picker = 1
+      " Map Rnvimr action
+      let g:rnvimr_action = {
+                  \ '<C-t>': 'NvimEdit tabedit',
+                  \ '<C-x>': 'NvimEdit split',
+                  \ '<C-v>': 'NvimEdit vsplit',
+                  \ 'gw': 'JumpNvimCwd',
+                  \ 'yw': 'EmitRangerCwd'
+                  \ }
       " Enable solarized color scheme
       syntax enable
       set termguicolors
       set background=dark
       colorscheme gruvbox
+      " flake8
+      let g:flake8_show_in_gutter=1
+      let g:flake8_show_quickfix=1
     '';
     plugins = with pkgs.vimPlugins; [
       gruvbox-community
       deoplete-nvim
       deoplete-jedi
       fzf-vim
-      ranger-vim
+      rnvimr
       tagbar
       vim-flake8
       vim-fugitive
       vim-obsession
     ];
-    extraPackages = [ pkgs.universal-ctags ];
-    extraPython3Packages = ps: with ps; [ black flake8 jedi pynvim ];
+    extraPackages = with pkgs; [ pkgs.python3Packages.flake8 universal-ctags ];
+    extraPython3Packages = ps: with ps; [ black jedi pynvim ];
   };
 }
